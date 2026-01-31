8e18fd48f7c85b9bbfbbe135e83e67894a42afea
Elliott Feltham <elliottfeltham98@gmail.com>
2026-01-31 22:38:12 +0000

    Add typscript, python and c/cpp intergration

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -147,7 +147,7 @@ vim.o.splitbelow = true
 --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
 --   See `:help lua-options`
 --   and `:help lua-guide-options`
-vim.o.list = true
+vim.o.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
@@ -163,6 +163,11 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = false
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -256,7 +261,7 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added via a link or github org/name. To run setup automatically, use `opts = {}`
-  { 'NMAC427/guess-indent.nvim', opts = {} },
+  --{ 'NMAC427/guess-indent.nvim', opts = {} },
 
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
@@ -483,6 +488,8 @@ require('lazy').setup({
       { 'mason-org/mason.nvim', opts = {} },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
+      { 'mason-org/mason-lspconfig.nvim', opts = {} },
+
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
 
@@ -583,6 +590,15 @@ require('lazy').setup({
         end,
       })
 
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client and client.name == 'ruff' then client.server_capabilities.hoverProvider = false end
+        end,
+        desc = 'LSP: Disable hover capability from Ruff (use Pyright hover instead)',
+      })
+
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
       --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
@@ -593,16 +609,48 @@ require('lazy').setup({
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --  See `:help lsp-config` for information about keys and how to configure
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        -- C/C++
+        clangd = {
+          filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
+          cmd = {
+            'clangd',
+          },
+        },
+
+        -- Python typing/intellisense
+        pyright = {
+          settings = {
+            pyright = {
+              -- If you use Ruff to organize imports, keep this true to avoid conflicts
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = 'workspace',
+              },
+            },
+          },
+        },
+
+        -- Python linting/formatting/code-actions (Ruff Language Server)
+        ruff = {
+          init_options = {
+            settings = {
+              -- Ruff server settings go here if you want (logLevel, etc.)
+            },
+          },
+        },
+
+        -- Typescript
+        ts_ls = {
+          filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+        },
+
+        eslint = {
+          filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+        },
       }
 
       -- Ensure the servers and tools above are installed
@@ -612,13 +660,19 @@ require('lazy').setup({
       --    :Mason
       --
       -- You can press `g?` for help in this menu.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'lua_ls', -- Lua Language server
-        'stylua', -- Used to format Lua code
-        -- You can add other tools here that you want Mason to install
-      })
 
+      local ensure_installed = vim.tbl_keys(servers or {})
+
+      vim.list_extend(ensure_installed, {
+        'lua_ls',
+        'stylua',
+
+        -- formatters
+        'prettierd',
+        'prettier',
+        'ruff',
+        'eslint_d',
+      })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       for name, server in pairs(servers) do
@@ -668,29 +722,92 @@ require('lazy').setup({
         desc = '[F]ormat buffer',
       },
     },
+
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
+        local disable_filetypes = {}
+        if disable_filetypes[vim.bo[bufnr].filetype] then return end
+        return { timeout_ms = 500, lsp_format = 'fallback' }
       end,
+
+      -- --- Prettier logic: project config wins, else global config ---
+      formatters = {
+        prettierd = {
+          stdin = true,
+          -- prettierd ONLY wants the file path
+          args = function(_, ctx) return { ctx.filename } end,
+          env = function(_, ctx)
+            local global = vim.fn.expand '~/.config/prettier/prettier.json'
+            if not vim.uv.fs_stat(global) then return {} end
+
+            local project_cfg = vim.fs.find({
+              '.prettierrc',
+              '.prettierrc.json',
+              '.prettierrc.yml',
+              '.prettierrc.yaml',
+              '.prettierrc.js',
+              '.prettierrc.cjs',
+              'prettier.config.js',
+              'prettier.config.cjs',
+            }, { upward = true, path = ctx.dirname })[1]
+
+            -- If project config exists: let prettierd/prettier resolve it normally
+            if project_cfg then return {} end
+
+            -- Otherwise: force your global config
+            return { PRETTIERD_DEFAULT_CONFIG = global }
+          end,
+        },
+
+        prettier = {
+          stdin = true,
+          args = function(_, ctx)
+            local global = vim.fn.expand '~/.config/prettier/prettier.json'
+
+            local project_cfg = vim.fs.find({
+              '.prettierrc',
+              '.prettierrc.json',
+              '.prettierrc.yml',
+              '.prettierrc.yaml',
+              '.prettierrc.js',
+              '.prettierrc.cjs',
+              'prettier.config.js',
+              'prettier.config.cjs',
+            }, { upward = true, path = ctx.dirname })[1]
+
+            -- Project config exists -> do NOT pass --config (so project overrides)
+            if project_cfg then return { '--stdin-filepath', ctx.filename } end
+
+            -- No project config -> use global if present
+            if vim.uv.fs_stat(global) then return { '--config', global, '--stdin-filepath', ctx.filename } end
+
+            return { '--stdin-filepath', ctx.filename }
+          end,
+        },
+      },
+
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+
+        c = { 'clang_format' },
+        cpp = { 'clang_format' },
+
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+
+        json = { 'prettierd', 'prettier', stop_after_first = true },
+        css = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
+        markdown = { 'prettierd', 'prettier', stop_after_first = true },
+
+        python = {
+          'ruff_fix',
+          'ruff_format',
+          'ruff_organize_imports',
+        },
       },
     },
   },
@@ -788,6 +905,8 @@ require('lazy').setup({
     },
   },
 
+  --THEMES
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -806,7 +925,35 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      --vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+
+  { -- Catppuccin
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'mocha', -- latte, frappe, macchiato, mocha
+        styles = {
+          comments = {}, -- no italics
+        },
+
+        auto_integrations = true,
+
+        integrations = {
+          treesitter = true,
+          telescope = { enabled = true },
+          gitsigns = true,
+          native_lsp = { enabled = true },
+          mini = { enabled = true },
+          blink_cmp = { style = 'bordered' },
+        },
+      }
+
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
@@ -851,21 +998,41 @@ require('lazy').setup({
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    config = function()
-      local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
-      require('nvim-treesitter').install(filetypes)
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = filetypes,
-        callback = function() vim.treesitter.start() end,
-      })
-    end,
+    branch = 'master',
+    build = ':TSUpdate',
+    lazy = false, -- force it to load during startup
+    opts = {
+      ensure_installed = {
+        'lua',
+        'vim',
+        'vimdoc',
+        'query',
+        'c',
+        'cpp',
+        'javascript',
+        'typescript',
+        'tsx',
+        'json',
+        'html',
+        'css',
+        'python',
+        'bash',
+        'diff',
+        'markdown',
+        'markdown_inline',
+      },
+      auto_install = true,
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
+      indent = { enable = true },
+    },
+    config = function(_, opts) require('nvim-treesitter.configs').setup(opts) end,
   },
-
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
 
   -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
+  --
   --
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
@@ -873,15 +1040,15 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
@@ -909,5 +1076,28 @@ require('lazy').setup({
   },
 })
 
+local function term_run(cmd) vim.cmd('botright split | resize 12 | terminal ' .. cmd) end
+
+local function project_python()
+  local root = vim.fn.getcwd()
+  if vim.env.VIRTUAL_ENV and vim.env.VIRTUAL_ENV ~= '' then
+    local vpy = vim.env.VIRTUAL_ENV .. '/bin/python'
+    if vim.uv.fs_stat(vpy) then return vpy end
+  end
+  local py = root .. '/.venv/bin/python'
+  if vim.uv.fs_stat(py) then return py end
+  py = root .. '/venv/bin/python'
+  if vim.uv.fs_stat(py) then return py end
+  return 'python3'
+end
+
+vim.keymap.set('n', '<leader>rp', function()
+  local file = vim.fn.expand '%:p'
+  term_run(project_python() .. ' ' .. vim.fn.shellescape(file))
+end, { desc = '[R]un [P]ython file' })
+
+vim.keymap.set('n', '<leader>rt', function() term_run 'pytest -q' end, { desc = '[R]un [T]ests (pytest)' })
+
+vim.keymap.set('n', '<leader>cb', function() term_run 'cmake --build build' end, { desc = '[C]++ [B]uild' })
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
